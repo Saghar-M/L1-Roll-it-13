81ef76188b0be1272595814a85397bff7a3d0f3a
Salim-Kh <khairis24982@masseyhigh.school.nz>
2026-02-04 12:26:49 +1300

    Simple yes/no checker(incomplete)

---
+++ C_01_yes_no_basics.py
@@ -0,0 +1,13 @@
+want_instructions = input("DO you want instructions? ")
+
+# check the user says yes/no
+if want_instructions == "yes" or want_instructions == "y":
+    print("you said yes")
+elif want_instructions == "no" or want_instructions == "n":
+    print("you said no")
+else:
+    print("please enter yes/no")
+
+
+
+
